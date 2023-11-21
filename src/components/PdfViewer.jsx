efdb08c8448fb0fd60389eb06edafa385d90b4db
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-11-21 11:38:14 +0530

    pdf viewer

---
+++ src/components/PdfViewer.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { pdfjs } from 'react-pdf';
+
+const PdfViewer = () => {
+  const [file, setFile] = useState(null);
+
+  const onFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(URL.createObjectURL(selectedFile));
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".pdf" onChange={onFileChange} />
+      {file && (
+        <div style={{ width: '100%', height: '500px' }}>
+          <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}>
+            <Viewer fileUrl={file} />
+          </Worker>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PdfViewer;
